57a596d57ff5b59ad2ac7ebcd1b19c92776d7c15
gondecu <gon.dc98@gmail.com>
2022-01-25 11:01:03 -0300

    first commit

---
+++ calculadora.js
@@ -0,0 +1,9 @@
+const sumar = require("./sumar")
+const restar = require("./restar")
+const multiplicar = require("./multiplicar")
+const dividir = require("./dividir")
+
+console.log(sumar(2,3))
+console.log(restar(4,2))
+console.log(multiplicar(2,2))
+console.log(dividir(4,0))
---
+++ multiplicar.js
@@ -0,0 +1,7 @@
+function multiplicar(numeroA, numeroB){
+    if (numeroA == 0 || numeroB == 0){
+        return 0
+    } else {
+        return numeroA * numeroB
+    }}
+module.exports = multiplicar
